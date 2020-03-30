17b7fae4f0234c51dc22d2362aad812f5e92b3c6
Jeppe Hauman <jeppe_hauman@hotmail.com>
2020-03-30 13:31:02 +0200

    Initial commit

---
+++ Axe.cs
@@ -0,0 +1,42 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Axe. An Axe is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Axe : Weapon
+    {
+        public const int InitialAxeMinDamage = 50;
+        public const int InitialAxeMaxDamage = 50;
+
+        #region Constructor
+        public Axe(string description) 
+            : base(description, InitialAxeMinDamage, InitialAxeMaxDamage)
+        {
+        } 
+        #endregion
+
+        #region Methods
+        int result;
+        public int DamageFromAxe()
+        {
+            result = CalculateDamage();
+            Desharpen();
+            return result;
+            
+        }
+        int decreaseByThree = 3;
+        public void Desharpen()
+        {
+            MinDamage = MinDamage - decreaseByThree;
+            MaxDamage = MaxDamage - decreaseByThree;
+        }
+
+        public void Sharpen()
+        {
+            MinDamage = InitialAxeMinDamage;
+            MaxDamage = InitialAxeMaxDamage;
+        }
+        #endregion
+    }
+}
---
+++ Wand.cs
@@ -0,0 +1,43 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Wand. A Wand is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Wand : Weapon
+    {
+        public const int InitialWandMinDamage = 10;
+        public const int InitialWandMaxDamage = 30;
+
+        private bool _isEnchanted;
+
+        #region Constructor
+        public Wand(string description) 
+            : base(description, InitialWandMinDamage, InitialWandMaxDamage)
+        {
+            _isEnchanted = false;
+        } 
+        #endregion
+
+        #region Properties
+        public bool IsEnchanted
+        {
+            get {return _isEnchanted;}
+            set {_isEnchanted = value;}
+        }
+            
+        #endregion
+
+        public int DamageFromWand()
+        {
+            if (IsEnchanted == true)
+            {
+                return (CalculateDamage()*2);
+            }
+            else
+            {
+                return (CalculateDamage());
+            }
+        }
+    }
+}
